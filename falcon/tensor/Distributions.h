30a5a106688f661e2583f1230d7c6755c576ec8c
lilanka <pathirage.lilanka@gmail.com>
2021-12-20 22:18:52 +0530

    uniform tensor error fix

--- falcon/tensor/Distributions.h
+++ falcon/tensor/Distributions.h
@@ -15,12 +15,12 @@ Tensor zeros(int d1=1, int d2=1, int d3=1, int d4=1, bool requires_grad=true) {
   return Tensor(af::constant(0, d1, d2, d3, d4), requires_grad);
 }
 
-Tensor uniform(int low, int high, int d1=1, int d2=1, int d3=1, int d4=1, bool requires_grad=true) {
-  return normal(d1, d2, d3, d4, requires_grad) * (low - high) + high;
-}
-
 Tensor ones(int d1=1, int d2=1, int d3=1, int d4=1, bool requires_grad=true) {
   return Tensor(af::constant(1, d1, d2, d3, d4), requires_grad);
+}
+
+Tensor uniform(int low, int high, int d1=1, int d2=1, int d3=1, int d4=1, bool requires_grad=true) {
+  return normal(d1, d2, d3, d4, requires_grad) * (low - high) + ones(d1, d2, d3, d4, requires_grad) * high ;
 }
 
 } // namespace Falcon
