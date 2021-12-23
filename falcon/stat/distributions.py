0be448d723bead6ff8a008972c06554ae2a957f8
lilanka <pathirage.lilanka@gmail.com>
2021-12-24 01:02:07 +0530

    stat

---
+++ falcon/stat/distributions.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+
+from utils import Utils 
+
+class Distributions(object):
+  
+  @classmethod
+  def binomial(self, n: int, y: int, data: list) -> list:
+    return self._ncr(n, y) * (data**y) * (1 - data)**(n-y) 
+ 
+  @classmethod
+  def beta(self, 
+
+  def _ncr(n: int, r: int) -> float:
+    return math.factorial(n)/(math.factorial(n-r) * math.factorial(r))       
